94eda9c4513e601e3d73415953e2e1142058ff8f
KIRAN-BUSARI <kiranbusari2208@gmail.com>
2024-09-20 18:01:16 +0530

    Add Navbar fix

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,5 @@
 'use client';
+
 import React, { useState } from 'react';
 import { cn } from '@/lib/utils';
 import { ModeToggle } from './ui/ModeToggle';
@@ -6,6 +7,11 @@ import Link from 'next/link';
 
 export function Navbar({ className }: { className?: string }) {
   const [active, setActive] = useState<string | null>(null);
+
+  const handleSetActive = (link: string) => {
+    setActive(link);
+  };
+
   return (
     <div
       className={cn(
@@ -15,9 +21,36 @@ export function Navbar({ className }: { className?: string }) {
     >
       <div className="mx-auto flex w-full items-center justify-between rounded-2xl border border-primary/10 bg-secondary/15 p-6 shadow-lg shadow-neutral-600/5 backdrop-blur-lg">
         <div className="flex space-x-6">
-          <Link href="/">Home</Link>
-          <Link href="/">Contributions</Link>
-          <Link href="/">Projects</Link>
+          <Link href="/" passHref>
+            <div
+              onClick={() => handleSetActive('home')}
+              className={cn(
+                active === 'home' ? 'text-blue-200' : 'text-gray-100',
+              )}
+            >
+              Home
+            </div>
+          </Link>
+          <Link href="/contributions" passHref>
+            <div
+              onClick={() => handleSetActive('contributions')}
+              className={cn(
+                active === 'contributions' ? 'text-blue-200' : 'text-gray-100',
+              )}
+            >
+              Contributions
+            </div>
+          </Link>
+          <Link href="/projects" passHref>
+            <div
+              onClick={() => handleSetActive('projects')}
+              className={cn(
+                active === 'projects' ? 'text-blue-200' : 'text-gray-100',
+              )}
+            >
+              Projects
+            </div>
+          </Link>
         </div>
         <div className="flex items-center">
           <ModeToggle />
